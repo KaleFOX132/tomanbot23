8b67940b3ef16b9d2ee55145031cb04f9ea7c57a
KaleFOX132 <124541542+KaleFOX132@users.noreply.github.com>
2023-02-06 08:15:17 -0400

    Add files via upload

---
+++ TomanBot/config.py
@@ -0,0 +1,14 @@
+from TomanBot.sample_config import Config
+
+class Development(Config):
+    OWNER_ID=
+    OWNER_USERNAME=Remnashi
+    API_KEY=RWDRIS-BMPHHE-RRCZAZ-AYKMIF-ARQ
+    DB_URI=something://somewhat:user@hosturl:port/njhhiwrv
+    MESSAGE_DUMP=-771072750
+    USE_MESSAGE_DUMP=True
+    SUDO_USERS=921396580, 954299697, 5010209835, 5676170494, 5492224551, 5516142677, 1152257449
+    LOAD=/start,/help,/info,/id
+    NO_LOAD=['translation']
+    TOKEN=5535045714:AAH6kP7yHftXTNSGbV_WEcdhNrmAY9-Sk-U
+    
